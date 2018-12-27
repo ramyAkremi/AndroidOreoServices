576534d32e661c66af8c302d4b9e5b897b1ec668
ramy.akremi <ramy.akremi@com-and-dev.com>
2018-12-27 15:28:31 +0100

    fix bug

--- app/src/main/java/com/ramydev/jobscheduler/workManager/WorkActivity.java
+++ app/src/main/java/com/ramydev/jobscheduler/workManager/WorkActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.ramydev.jobscheduler.R;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import androidx.work.Constraints;
 import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
@@ -25,9 +27,17 @@ public class WorkActivity extends AppCompatActivity {
         Constraints constraints = new Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build();
-        //OneTimeWorkRequest
-        /*OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest
+        //OneTimeWorkRequest at specific time
+        /*Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        long atMidNight = (c.getTimeInMillis() - System.currentTimeMillis());
+        OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest
                 .Builder(MyWorkManager.class)
+                .setInitialDelay(atMidNight, TimeUnit.MILLISECONDS)
                 .setConstraints(constraints)
                 .build();*/
         //PeriodicWorkRequest
@@ -35,6 +45,7 @@ public class WorkActivity extends AppCompatActivity {
                 .Builder(MyWorkManager.class, 15, TimeUnit.MINUTES)
                 .setConstraints(constraints)
                 .build();
+
         WorkManager.getInstance().enqueue(periodicWorkRequest);
     }
 }
